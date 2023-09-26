93e158e04c8ad68f5dcbfb47410347057ef2324d
eduhdev <eduardocarvalho970@gmail.com>
2023-09-26 17:43:33 -0300

    Create Invoices list with mock data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,42 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { invoicesMock } from '@/lib/mocks';
+import { formatToPrice } from '@/lib/utils';
+
 const Dashboard = () => {
-  return <h1>Dashboard Page</h1>;
+  return (
+    <div className='flex-1 pt-8 px-4'>
+      <h1 className='text-4xl font-semibold'>Invoices List</h1>
+      <Table className='mt-6'>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className='w-[100px]'>Invoice</TableHead>
+            <TableHead>Client Name</TableHead>
+            <TableHead>Total Amount</TableHead>
+            <TableHead className='text-right'>Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoicesMock.map((invoice) => (
+            <TableRow key={invoice.id}>
+              <TableCell className='font-medium'>INV-{invoice.id}</TableCell>
+              <TableCell>{invoice.client_name}</TableCell>
+              <TableCell>{formatToPrice(invoice.total_amount)}</TableCell>
+              <TableCell className={`text-right ${invoice.status === "Paid" ? "text-green-600" : "text-red-700"}`}>{invoice.status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/lib/mocks.ts
@@ -0,0 +1,33 @@
+export const invoicesMock = [
+    {
+      "id": 1,
+      "client_name": "Client 1",
+      "total_amount": 100,
+      "status": "Paid"
+    },
+    {
+      "id": 2,
+      "client_name": "Client 2",
+      "total_amount": 200,
+      "status": "Unpaid"
+    },
+    {
+      "id": 3,
+      "client_name": "Client 3",
+      "total_amount": 300,
+      "status": "Paid"
+    },
+    {
+      "id": 4,
+      "client_name": "Client 4",
+      "total_amount": 400,
+      "status": "Unpaid"
+    },
+    {
+      "id": 5,
+      "client_name": "Client 5",
+      "total_amount": 500,
+      "status": "Paid"
+    }
+  ]
+  
